2442c247222d8087bdf9d866764a5f991abd06bc
Animesh Dwivedi <animeshdwivedinwm@gmail.com>
2020-06-20 16:56:38 -0500

    Some more updates.

--- reactplayground/src/App.js
+++ reactplayground/src/App.js
@@ -8,15 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React Playground with hooks and stuff. Slowly Redux will be added and it will be fun. Click below to enjoy.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a href=" " onClick={() => {alert("In Progress")}}>
+          In Progress
         </a>
       </header>
     </div>
