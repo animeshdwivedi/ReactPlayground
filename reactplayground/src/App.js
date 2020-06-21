ea861470399a309a2d88abc54a5c8f170d8f0090
Animesh Dwivedi <animeshdwivedinwm@gmail.com>
2020-06-20 22:27:55 -0500

    Adding state clear changes -implementing state clear changes for both functional and class components. -whitespace changes

--- reactplayground/src/App.js
+++ reactplayground/src/App.js
@@ -8,21 +8,21 @@ import 'bootstrap';
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-light" style={{"background-color": "#e3f2fd"}}>
+      <nav className="navbar navbar-light" style={{ "background-color": "#e3f2fd" }}>
         <p>
-            Animesh Dwivedi says: Its just basic, but its helpfull.
+          Animesh Dwivedi says: Its just basic, but its helpfull.
         </p>
       </nav>
       <div className="container-fluid">
-          <p>
-            React Playground with hooks and stuff.
+        <p>
+          React Playground with hooks and stuff.
           </p>
-          <div>
-            <p>Below is an example of useState hook</p>
-            <InputElementUseState />
-            <p>Below is the equivalent Class Component</p>
-            <InputElementClass />
-          </div>
+        <div>
+          <p>Below is an example of useState hook</p>
+          <InputElementUseState />
+          <p>Below is the equivalent Class Component</p>
+          <InputElementClass />
+        </div>
 
       </div>
     </div>
